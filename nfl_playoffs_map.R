51dfc174e8837998aa53c73de17697c3786aac67
John Sugden <john.sugden@cerner.com>
2017-01-10 15:02:04 -0600

    Update nfl_playoffs_map.R

--- nfl_playoffs_map.R
+++ nfl_playoffs_map.R
@@ -1,6 +1,6 @@
 
 library(leaflet)
-m <- data.table(rbind(c("Boston", "Patriots", "AFC", "Gillette Stadium", 1, 42.0909, -71.2643), 
+m <- data.table(rbind(c("New England", "Patriots", "AFC", "Gillette Stadium", 1, 42.0909, -71.2643), 
                       c("Kansas City", "Chiefs", "AFC", "Arrowhead Stadium", 2, 39.0489, -94.4839), 
                       c("Pittsburgh", "Steelers", "AFC", "Heinz Field", 3, 40.4468, -80.0158), 
                       c("Houston", "Texans", "AFC", "NRG Stadium", 4, 29.6847, -95.4107), 
@@ -13,4 +13,13 @@ m <- data.table(rbind(c("Boston", "Patriots", "AFC", "Gillette Stadium", 1, 42.0
                       c("New York", "Giants", "NFC", "MetLife Stadium", 5, 40.8128, -74.0742), 
                       c("Detroit", "Lions", "NFC", "Ford Field", 6, 42.3400, -83.0456)))
 names(m) <- c("City", "Team", "League", "Stadium", "Seed", "Lat", "Long")
-leaflet(m) %>% addTiles() %>% setView(lat=40, lng=-98, zoom=4) %>% addCircleMarkers(lng= ~Long, lat= ~Lat)
+m[, active:=T]
+m[City %in% c("Oakland", "Miami", "Detroit", "New York"), active:=F] ## wildcard
+m[City %in% c("", "", "", ""), active:=F] ## divisional
+m[City %in% c("", ""), active:=F] ## conference
+m[City %in% c(""), active:=F] ## title
+leaflet(m) %>% 
+  addTiles() %>% 
+  setView(lat=40, lng=-98, zoom=4) %>% 
+  addCircleMarkers(data=m[active==T], lng= ~Long, lat= ~Lat) %>% 
+  addCircleMarkers(data=m[active==F], lng= ~Long, lat= ~Lat, col="red")
